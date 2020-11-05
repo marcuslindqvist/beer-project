7246897141cec3acd80d2c1d3bb77335502a82c6
marcuslindqvist <marcus.lindqvist@iths.se>
2020-11-05 12:24:20 +0100

    Update index.js

---
+++ index.js
@@ -0,0 +1,34 @@
+const api = "https://api.punkapi.com/v2/beers";
+const formElement = document.querySelector("form");
+const mainElement = document.querySelector("div.main");
+
+formElement.addEventListener("submit", onSubmit);
+
+function getData(url, callback) {
+  fetch(url)
+    .then((res) => res.json())
+    .then((data) => {
+      callback(data);
+    })
+    .catch((error) => console.log(error));
+}
+
+function render(data) {
+  const ulElement = document.createElement("ul");
+  ulElement.addEventListener("click", onUlClicked);
+  for (let i = 0; i < data.length; i++) {
+    const beer = data[i];
+
+    const liElement = document.createElement("li");
+    liElement.setAttribute("name", beer.id);
+    liElement.textContent = beer.name;
+    ulElement.appendChild(liElement);
+  }
+  mainElement.appendChild(ulElement);
+}
+
+function onUlClicked(evt) {
+  const id = evt.target.getAttribute("name");
+  const url = `info.html?name=${id}`;
+  document.location.href = url;
+}
