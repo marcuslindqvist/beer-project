4f380612536b859809487c856d0bce83c79158d5
marcuslindqvist <marcus.lindqvist@iths.se>
2020-11-06 13:57:43 +0100

    flik
    flikarna ligger nu lite under varandra.
    har kopierat in maddys kod för random-funktionalitet i INDEX men har inte kopplat ihop dem

--- index.js
+++ index.js
@@ -32,3 +32,49 @@ function onUlClicked(evt) {
   const url = `info.html?name=${id}`;
   document.location.href = url;
 }
+
+/*Följande e maddys randomkod
+const rutaElement = document.querySelector(".ruta");
+const api = "https://api.punkapi.com/v2/beers/random";
+const onBtn = document.querySelector("button");
+("image_url"); //hänvisning till kodraden
+
+onBtn.addEventListener("click", getData);
+
+function getData() {
+  while (rutaElement.firstChild) {
+    rutaElement.removeChild(rutaElement.firstChild);
+  }
+  fetch(api)
+    .then((res) => res.json())
+    .then((data) => {
+      render(data);
+
+      console.log(data[0].id);
+    });
+}
+
+function render(data) {
+  const beer = data[0];
+  const name = beer.name;
+  const image = beer.image_url;
+
+  const h1Tag = document.createElement("h1");
+  const img = document.createElement("img");
+
+  h1Tag.textContent = name;
+  img.src = image;
+
+  rutaElement.appendChild(h1Tag);
+  rutaElement.appendChild(img);
+}
+
+function onUlClicked(evt) {
+  /* function see more för att komma till en annan sida där mer info
+ om ölen visas upp
+
+  const id = evt.target.getAttribute("name");
+  const url = `myView.html?name=${id}`;
+  document.location.href = url;
+}
+*/
